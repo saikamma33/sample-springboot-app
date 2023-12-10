faa33bc3436d6dec4c6e40ce026cd3f5742fcaab
saikamma33 <bhaskarkamma3@gmail.com>
2023-12-10 12:19:18 -0600

    adding info

--- Test.java
+++ Test.java
@@ -1,2 +1,22 @@
 public class Test {
+    int age = 26;
+   static String name = " Sai ";
+
+    public static void main (String [] args){
+
+        Test ts = new Test();
+        String lastname = " kamma ";
+
+        System.out.println(ts.age);
+        System.out.println(lastname);
+        System.out.println(name);
+sample();
+    }
+ public static void sample(){
+         String phonenum = " 9010314822 " ;
+         double d = 3.7;
+         System.out.println(phonenum);
+         System.out.println(d);
+ }
+
 }
